f1779d739db6c102c50e6176f5a4f6a0ec3e51d9
gardiner nardini <gardiner.nardini@gmail.com>
2018-09-23 15:41:57 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { Component } from 'react'
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            astronauts:null
+        }
+    }
+
+
+    render(){
+        
+        return(
+            <div>
+                {this.state.astronauts}
+            </div>
+        );
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res=>res.json())
+        
+    }
+
+
+} // end of App class
+
+export default App
