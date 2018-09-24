eb8bd91b704c863d49d5d8b463c0d910da69842e
yunglleung1 <yungl.leung1@gmail.com>
2018-09-24 14:15:21 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      peopleDetails: ''
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        Hello
+        {this.state.peopleDetails}
+      </div>
+    )
+  }
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(data => {
+      let people = data.people
+
+      let peopleDetails = people.map((person) => {
+        return <div key={person.name}>{person.name + ' has a craft of ' + person.craft + '.'}</div>
+      })
+
+      this.setState({
+        peopleDetails: peopleDetails
+      })
+      // console.log(data.people)
+    })
+  }
+}
 // create your App component here
+
+export default App
